3ce2f6fda54f7cd67021ba0c5f4f64a41f44c259
Alessia <alessia.marzano@gmail.com>
2026-01-26 18:23:12 +0100

    rihiesta evento, evento, pomotori,rappresentanti

---
+++ Database/PythonGenerators/Rihiesta_rievimento.py
@@ -0,0 +1,17 @@
+create table Richiesta_Ricevimento (
+     Codice numeric(10) not null,
+     Inserimento TINYINT(1) not null,
+     Matricola numeric(10) not null,
+     Codice_Ric numeric(10),
+     N_Slot numeric(3),
+     Ricevimento numeric(10) not null,
+     constraint ID_Richiesta_Ricevimento_ID primary key (Codice));
+
+
+     allora inserimento o è vero (1) o è falso (0) anella
+     se è di anella i vuole il odie slot e odie rievimento Ricevimento
+
+     se è di inserimento solo rievimento 
+
+
+     matriola è studente he rihiede 
